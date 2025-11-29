6992988c0ba23a74ca2c83c8bba85aa569e39f2c
o-vuong <oscarvuong@gmail.com>
2025-11-29 13:04:49 -0800

    refactor(layout): update root layout and header components

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -7,6 +7,7 @@ import {
 } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import Header from "../components/Header";
+import { MigrationHandler } from "../components/auth/MigrationHandler";
 import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
 import StoreDevtools from "../lib/demo-store-devtools";
 import appCss from "../styles/globals.css?url";
@@ -47,6 +48,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 				<HeadContent />
 			</head>
 			<body>
+				<MigrationHandler />
 				<Header />
 				{children}
 				<TanStackDevtools
